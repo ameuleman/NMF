c0facdb464712f031f50c655d60d98f8a94610dc
Meuleman Andreas <ameuleman@asi-testing>
2016-12-04 19:07:04 +0100

    Amélioration du rendu final

--- prog_color.m
+++ prog_color.m
@@ -25,7 +25,7 @@ V = double(V);
 r = 2;
 
 %sparceness
-sH1 = 0.01;
+sH1 = 0.001;
 sH2 = 0.4;
 
 %initialisation de W et H
@@ -33,7 +33,9 @@ W = rand(n,r);
 H = rand(r,p);
 
 L1 = (1-sH1)*sqrt(p)+sH1;
-H(1,:) = projeter(H(1,:),L1,1);
+for l = 1:(r-1)
+    H(l,:) = projeter(H(l,:),L1,1);
+end
 L1 = (1-sH2)*sqrt(p)+sH1;
 H(2,:) = projeter(H(2,:),L1,1);
     
@@ -47,7 +49,7 @@ epsH = 1e-5;
 iter = 0
 muW = 0.85;
 muH = 0.85;
-while iter<1500%norm(W-WOld) > epsW || norm(H-HOld) > epsH
+while iter<500
     W = W.*(V*H')./(W*H*H');
     
     HOld = H;
@@ -59,9 +61,11 @@ while iter<1500%norm(W-WOld) > epsW || norm(H-HOld) > epsH
         H = H-muH*W'*(W*H-V);
         %projection
         L1 = (1-sH1)*sqrt(p)+sH1;
-        H(1,:) = projeter(H(1,:),L1,1);
+        for l = 1:(r-1)
+            H(l,:) = projeter(H(l,:),L1,1);
+        end
         L1 = (1-sH2)*sqrt(p)+sH1;
-        H(2,:) = projeter(H(2,:),L1,1);
+        H(r,:) = projeter(H(r,:),L1,1);
     
             
         if norm(V-W*H) < obj || muH < 1e-200
@@ -75,17 +79,21 @@ end
 
 
 %Séparation de la composante diffuse et spéculaire de l'image
-WD = W(:,1);
-WS = W(:,2);
 
-HD = H(1,:);
-HS = H(2,:);
+for i = 1:(r-1)
+    WD(:,i) = W(:,i);
+    HD(i,:) = H(i,:);
+end
+WS = W(:,r);
+
+HS = H(r,:);
 
 RD = (WD*HD);
 RS = (WS*HS);
 
-%Reconstruction de l'image en matrice de tableau
+%Reconstruction de l'image en matrice de tableaux
 k = 0;
+
 for i = 1:nI
     for j = 1:pI
         k = k+1;
@@ -93,6 +101,8 @@ for i = 1:nI
         RIS(i,j,:) = RS(:,k); 
     end
 end
+
+CIS = RID.*RIS./255;
 
 %affichage pour les tests
 
@@ -111,12 +121,9 @@ imshow(uint8(RIS))
 title('Composante spéculaire')
 
 subplot(1,5,4)
-imshow(uint8(RIS + RID))
+imshow(uint8(RID+RIS))
 title('Image restituée')
 
 subplot(1,5,5)
-imshow(uint8(double(I)-0.5*RIS))
+imshow(uint8(CIS+double(I)-RIS))
 title('Image sans spécularité')
-
-title('Méthode NMF')
-
